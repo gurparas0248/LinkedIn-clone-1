fb751c5249bafa54e2678ef3155794947ae881c0
Gurparas Singh <gurparas0248.be20@chitkara.edu.in>
2022-09-13 16:56:02 +0530

    Added and styled Login header elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import {BrowserRouter as Router, Switch , Route} from "react-router-dom";
+
+
 import './App.css';
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h2>Lets build linkedin clone</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
+
+
+
 
 export default App;
